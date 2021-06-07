5b3711f50ef3245fa46ac15a337ce3afb21d0958
Aadya-Maheshwari <76033806+Aadya-Maheshwari@users.noreply.github.com>
2021-06-07 15:18:53 +0530

    Add files via upload

---
+++ Waterdrop.js
@@ -0,0 +1,17 @@
+class Waterdrop{
+    constructor(x,y){
+       var option={
+           
+           restitution:0.8,
+           friction:0.1,
+         
+       }
+        this.body=Bodies.circle(x,y,50,option);
+        this.radius=50;
+        World.add(world,this.body);
+    }
+    display(){
+        ellipseMode(CENTER);
+        ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
+    }
+}
